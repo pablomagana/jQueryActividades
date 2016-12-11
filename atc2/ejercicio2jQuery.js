8e5d2f1bd97ef3e1bdfca6a057f29294fc37c620
pablomagub <pablomagub@gmail.com>
2016-12-11 16:39:27 +0100

    actividad 2

--- atc2/ejercicio2jQuery.js
+++ atc2/ejercicio2jQuery.js
@@ -1,12 +1,12 @@
 //btnTitulo, btnLista, btnDelete
 // h1, aplicar estilo añadiendo la clase titulo
 $('#btnTitulo').click(function(){
-  $('h1').addClass("titulo");
+  $('h1').addClass("titulo",3000,"easeOutBounce");
 });
 
 //ul aplicar estilo añadiendo clase horizontal y sublista a los hijos de los elementos de la lista
 $('#btnLista').click(function(){
-  $('#obras > li').fadeIn('slow',function(){$(this).addClass("horizontal")});
+  $('#obras > li').addClass("horizontal");
   $('#obras *').not(".horizontal").addClass('sublista');
 });
 
