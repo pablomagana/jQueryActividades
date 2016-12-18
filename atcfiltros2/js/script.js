09dbeaa0900dbb98c248c600b552582d792297fe
pablo <pablomaganapablomagub@gmail.com>
2016-12-18 19:55:06 +0100

    actividad filtros jquery

--- atcfiltros2/js/script.js
+++ atcfiltros2/js/script.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
 //1. Selecciona todos los párrafos que tengan el atributo class y le cambias el color de fondo a amarillo.
-//$('p[class]').css('background-color','yellow');
+$('p[class]').css('background-color','yellow');
 //2. Selecciona todos los párrafos que tengan el atributo id con el valor “parrafo1” y le cambios el color de fondo a verde oscuro.
-//$('p[id="p1"]').css('background-color','red');
+$('p[id="p1"]').css('background-color','red');
 //3. Selecciona todos los párrafos cuyo id empiece como “par” y cambia la letra a verdana y negrita.
-//$('p[id^="par"]').css({'font-family':'verdana','font-weight':'bold'});
+$('p[id^="par"]').css({'font-family':'verdana','font-weight':'bold'});
 //4. Selecciona todos los párrafos cuya clase No sea “clase1” o los párrafos que no contengan el atributo class.
-//$("p[class!='clase1'] , p:not([class])").css("background-color","red");
+$("p[class!='clase1'] , p:not([class])").css("background-color","red");
 //5. Selecciona los párrafos que contienen en el id el valor “afo” y lo pondremos con una opacidad del 25% y el ancho de borde será de 200px (usar función animate).
-//$("p[id*='afo']").animate({opacity: 0.25},2000,function(){});
+$("p[id*='afo']").animate({opacity: 0.25},2000,function(){});
 //6. Selecciona todos los párrafos cuya clase empiece por “par” y tenga el atributo align con el contenido center y lo conviertes a mayúsculas.
-//$("p[class^='par'][align='center']").val().toUpperCase();
-alert(
-  $("p[class^='par'][align='center']").value
-);
-alert('bla');
+$("p[class^='par'][align='center']").css('text-transform', 'uppercase');
 //7. Oculta los párrafos cuyo id acaben con 1. Luego los vuelves a mostrar.
-
+$("p[id$='1']").fadeOut();
+$("p[id$='1']").fadeIn();
 //8. Selecciona todos los elementos que son del tipo cabecera (h1,h2 y h3).
-
+$(":header").css("background-color","red");
 //9. Selecciona el elemento con id pdf1, los encabezados h2 y los párrafos con el atributo align a center y que cuando cliques que aparezca el mensaje “Has clicado”.
-
-})
+$("*[id=p1],h2,p[align='center']").click(
+  function(){
+    alert("Has clicado");
+  });
+});
